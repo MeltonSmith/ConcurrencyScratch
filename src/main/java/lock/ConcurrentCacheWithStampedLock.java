8bcaf972ec3d340e886103ca2af3021504bb7e1f
Ian Rakhmatullin <Ian_Rakhmatullin@epam.com>
2021-12-07 02:36:20 +0300

    refactoring value of the cache into linkedList

--- src/main/java/lock/ConcurrentCacheWithStampedLock.java
+++ src/main/java/lock/ConcurrentCacheWithStampedLock.java
@@ -1,7 +1,9 @@
 package lock;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Stack;
 import java.util.concurrent.locks.StampedLock;
 
 /**
@@ -10,38 +12,45 @@ import java.util.concurrent.locks.StampedLock;
  */
 public class ConcurrentCacheWithStampedLock {
 
-    private final Map<String,Integer> map = new HashMap<>();
-    private StampedLock lock = new StampedLock();
+    private final Map<String, LinkedList<Integer>> map = new HashMap<>();
+    private final StampedLock lock = new StampedLock();
 
     public void put(String key, Integer value){
         long stamp = lock.writeLock();
         try {
-            map.put(key, value);
+            LinkedList<Integer> integers = map.computeIfAbsent(key, k -> new LinkedList<>());
+            if (integers.size() >= 2000){
+                integers.removeFirst();
+            }
+            integers.addLast(value);
         } finally {
             lock.unlockWrite(stamp);
         }
     }
 
+    /**
+     * peeks the last by the key
+     */
     public Integer get(String key) {
         long stamp = lock.readLock();
         try {
-            return map.get(key);
+            return map.get(key).peekLast();
         } finally {
             lock.unlockRead(stamp);
         }
     }
 
-    public Integer readOptimistically(String key){
+    public Integer getOptimistically(String key){
         long stamp = lock.tryOptimisticRead();
         System.out.println("Optimistic stamp is " + stamp);
-        Integer valueOptimistic = map.get(key);
+        Integer valueOptimistic = map.get(key).peekLast();
 
 
         if (!lock.validate(stamp)){
             stamp = lock.readLock();
             System.out.println("new stamp is ..." + stamp);
             try{
-                return map.get(key);
+                return map.get(key).peekLast();
             } finally {
                 lock.unlock(stamp);
             }
